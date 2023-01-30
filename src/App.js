285df7e130fb292510e11aeffd60e68b830f4ae5
patrickbrusven <pjbrusven@gmail.com>
2023-01-30 13:59:08 -0800

    Moved test data to its own file.

--- src/App.js
+++ src/App.js
@@ -3,33 +3,6 @@ import { useState } from "react";
 import EnterConstraints from "./EnterConstraints";
 import EnterStudent from "./EnterStudent";
 import BestCombination from "./BestCombination";
-const testData = [
-  {
-    name: "Jane",
-    earnings: 1000,
-    hours: 3,
-  },
-  {
-    name: "Bob",
-    earnings: 3000,
-    hours: 5,
-  },
-  {
-    name: "Mark",
-    earnings: 2700,
-    hours: 4,
-  },
-  {
-    name: "Jill",
-    earnings: 5000,
-    hours: 8,
-  },
-  {
-    name: "Don",
-    earnings: 3600,
-    hours: 5,
-  },
-];
 
 function App() {
   const [data, setData] = useState(null);
---
+++ src/testData.js
@@ -0,0 +1,29 @@
+const testData = [
+  {
+    name: "Jane",
+    earnings: 1000,
+    hours: 3,
+  },
+  {
+    name: "Bob",
+    earnings: 3000,
+    hours: 5,
+  },
+  {
+    name: "Mark",
+    earnings: 2700,
+    hours: 4,
+  },
+  {
+    name: "Jill",
+    earnings: 5000,
+    hours: 8,
+  },
+  {
+    name: "Don",
+    earnings: 3600,
+    hours: 5,
+  },
+];
+
+export default testData;
